599ae2fdd3562397f55816f66cf6d92ec4f28e83
jkcao <jkcao@users.noreply.github.com>
2018-01-04 20:30:44 -0800

    More jumping/physics fixes + relfection added!
    Jumping boolean check is fixed, jumping range is more accurate, should be absolutely no more sticking, and reflection/inverted-control character component added.

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+abstract public class Character : MonoBehaviour {
+
+	public float speed;
+	public float jumpHeight;
+	public float jumpBox;
+	protected Rigidbody2D rigidBody;
+	protected bool isGrounded;
+
+	// Use this for initialization
+	void Start () {
+		rigidBody = GetComponent<Rigidbody2D> ();
+		isGrounded = true;
+	}
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		// Check if player is grounded, via three points: its center and its two corners.
+		float end = jumpBox * 1.4f;
+		Vector2 leftCheck = new Vector2(transform.position.x - jumpBox, transform.position.y);
+		Vector2 rightCheck = new Vector2(transform.position.x + jumpBox, transform.position.y);
+		Vector2 midCheck = transform.position;
+		Vector2 leftEnd = leftCheck;
+		leftEnd.y -= end;
+		Vector2 rightEnd = rightCheck;
+		rightEnd.y -= end;
+		Vector2 midEnd = midCheck;
+		midEnd.y -= end;
+
+		if (Physics2D.Linecast (leftCheck, leftEnd, 1 << LayerMask.NameToLayer ("Ground"))
+			|| Physics2D.Linecast (rightCheck, rightEnd, 1 << LayerMask.NameToLayer ("Ground"))
+			|| Physics2D.Linecast (midCheck, midEnd, 1 << LayerMask.NameToLayer ("Ground"))) {
+			isGrounded = true;
+		} else {
+			isGrounded = false;
+		}
+
+		float horizontal = Input.GetAxis("Horizontal");
+
+		// Check if player can jump.
+		bool jump = false;
+		if (isGrounded && (Input.GetKeyDown ("space"))) {
+			jump = true;
+		}
+
+		// Call Movement function.
+		Movement (horizontal, jump);
+	}
+
+	protected abstract void Movement (float horizontal, bool jump);
+}
--- Assets/Scripts/Reflection.cs
+++ Assets/Scripts/Reflection.cs
@@ -2,13 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : Character {
+public class Reflection : Character {
 	protected override void Movement(float horizontal, bool jump) {
 		// Moves LR and jumps if input & able.
 		if (!jump) {
-			rigidBody.velocity = new Vector2 (speed * horizontal, rigidBody.velocity.y);
+			rigidBody.velocity = new Vector2 (-(speed * horizontal), rigidBody.velocity.y);
 		} else {
-			rigidBody.velocity = new Vector2 (speed * horizontal, jumpHeight);
+			rigidBody.velocity = new Vector2 (-(speed * horizontal), jumpHeight);
 			isGrounded = false;
 		}
 	}
