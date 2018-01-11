78b3085ecc0b2d256fe08611267f794e5e925f1a
Kimberly Lo <32209256+kimberll@users.noreply.github.com>
2018-01-10 16:12:35 -0800

    Add split screen
    Add split screen with two cameras

---
+++ Assets/FollowBehavior.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowBehavior : MonoBehaviour {
+
+	[SerializeField]
+	protected Transform trackingTarget;
+
+	[SerializeField]
+	float xOffset;
+
+	[SerializeField]
+	float yOffset;
+
+	// Update is called once per frame
+	void Update()
+	{
+		transform.position = new Vector3(trackingTarget.position.x + xOffset, 
+			trackingTarget.position.y + yOffset, transform.position.z);
+	}
+}
