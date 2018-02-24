5b86d00ad48e3b23c86274f3529e6283bed0ec3e
jkcao <jkcao@users.noreply.github.com>
2018-02-24 18:38:56 -0500

    Win state
    + other minor fixes and tweaks and organization

---
+++ Assets/Scripts/Win.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Win : MonoBehaviour {
+
+	protected void OnTriggerEnter2D (Collider2D col){
+		if (col.gameObject.tag == "Character" || col.gameObject.tag == "Reflection") {
+			Application.LoadLevel(Application.loadedLevel);
+		}
+	}
+}
