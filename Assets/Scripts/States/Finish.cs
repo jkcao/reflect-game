c66f29f0445417fa1293c7c3860187f82db30403
jkcao <jkcao@users.noreply.github.com>
2018-04-28 12:36:20 -0400

    Adding finish art + small level tweaks

---
+++ Assets/Scripts/States/Finish.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Finish : MonoBehaviour {
+
+	IEnumerator toMenu() {
+		float fadeTime = this.GetComponent<FadeIn> ().BeginFade (1);
+		yield return new WaitForSeconds(fadeTime);
+		SceneManager.LoadScene ("MainMenu");
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if(Input.anyKeyDown) StartCoroutine (toMenu());
+	}
+}
